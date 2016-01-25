aaa07af9e961eabec6d092a83a73156813c0d153
Anton Vanco <anton.vanco@rightster.com>
2016-01-25 10:14:53 +0000

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+enablePlugins(ScalaJSPlugin)
+
+name := "totallynotalie"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+
+    
---
+++ src/main/scala/net/thenobody/hootsuite/totallynotalie/Main.scala
@@ -0,0 +1,12 @@
+package net.thenobody.hootsuite.totallynotalie
+
+import scala.scalajs.js.JSApp
+
+/**
+  * Created by antonvanco on 25/01/2016.
+  */
+object Main extends JSApp {
+  def main(): Unit = {
+    println("Hello world!")
+  }
+}
